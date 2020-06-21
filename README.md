14adddb477518c37fee7cb1d3c24c4e3c5855e6c
Simran <simranjeet_s87@yahoo.com>
2020-06-21 12:57:51 +0530

    Styling added using CSS
    Beta stage

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Created using python, GTK for GUI, PickleDB database
 
 Original author: BT40
 
-Project started on 14 June 2020
+Project started on 14 June 2020, completed on 21 June 2020
 
 Main target OS: Ubuntu/Debian
 
--- gtkcounter.py
+++ gtkcounter.py
@@ -2,34 +2,48 @@ import sys
 import gi
 import pickledb
 
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gio
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
 
 
 class MainWindow(Gtk.ApplicationWindow):
 
     def __init__(self, App):
-        super(MainWindow, self).__init__(title="Counter", application=App)
+        super(MainWindow, self).__init__(title="Counter", application=App, decorated=True, name="csswindow")
         #self.set_default_size(280, 180)
         self.set_border_width(10)
+        
+        try:
+            self.set_icon_from_file("ortealicon512.png")
+            print("Icon loaded successfully")
+        except Exception as e:
+            print("Failed loading Icon")
+            print (e.message)
+            
+        
+              
+        
         
         self.db = pickledb.load('pycounter.db', False)
         savedprev=self.db.get('previous')
         self.count=int(savedprev)
         print("Database loaded successfully")
         
-        print("starting creating vbox")
+        #print("Starting creating vbox")
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.add(vbox)
         
         
         
-        self.countLabel = Gtk.Label(self.count)
+        self.countLabel = Gtk.Label(label=self.count)
+        self.countLabel.set_name('cntlab')
         vbox.pack_start(self.countLabel, True, True, 0)
 
         incbutton = Gtk.Button.new_with_label("Increase")
+        incbutton.set_name('incbut')
         incbutton.connect("clicked", self.increase)
         vbox.pack_start(incbutton, True, True, 0)
         
@@ -38,17 +52,52 @@ class MainWindow(Gtk.ApplicationWindow):
         vbox.pack_start(decbutton, True, True, 0)
         
         resetbutton = Gtk.Button.new_with_label("Reset")
+        resetbutton.set_name('resetbut')
         resetbutton.connect("clicked", self.resett)
         vbox.pack_start(resetbutton, True, True, 0)
         
         exitbutton = Gtk.Button.new_with_label("Exit")
+        exitbutton.set_name('exitbut')
         exitbutton.connect("clicked", self.exitt)
         vbox.pack_start(exitbutton, True, True, 0)
+        
+        
+        
+        
+        
+        style_provider = Gtk.CssProvider()
+        
+        css = """
+        
+
+        #incbut {
+            background-color: blue;
+            color: #306754;
+  	    font: 22px "Comic Sans";
+        }
+        
+        #resetbut {
+            background-color: red;
+            color: #C24641;
+  	    
+        }
+               
+	#cntlab {
+ 	    font: 50px Sans;
+ 	    color: #25383C;
+	}
+        """
+        style_provider.load_from_data(bytes(css.encode()))
+	
+        Gtk.StyleContext.add_provider_for_screen(
+            Gdk.Screen.get_default(), style_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
         
         print("GUI creating done")
         
     def increase(self, incbutton):
-        print('"Increased pressed')
+        print('Increase button pressed')
         count=self.count
         count=count+1
         self.count=count
@@ -58,7 +107,7 @@ class MainWindow(Gtk.ApplicationWindow):
 
         
     def decrease(self, decbutton):
-        print('"decreased pressed')
+        print('Decrease button pressed')
         count=self.count
         count=count-1
         self.count=count
@@ -67,7 +116,7 @@ class MainWindow(Gtk.ApplicationWindow):
         self.db.dump()
         
     def resett(self, resetbutton):
-        print('"Reset pressed')
+        print('Reset button pressed')
         self.count=0
         self.countLabel.set_text ("0");
         self.db.set('previous', '0')
@@ -77,10 +126,13 @@ class MainWindow(Gtk.ApplicationWindow):
             
     def exitt(self, exbutton):
     	app.quit()
-    	print("Successfully shut down")
+    	print("Count saved to database, Successfully shut down")
     	
-
-
+    
+    
+    
+    
+    
 
 class App(Gtk.Application):
     def __init__(self):
